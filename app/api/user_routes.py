1c0ae88bc92f951578a6dae435f49200c5d9fe12
Gustavo <g.ebenezer.miranda@gmail.com>
2021-07-30 12:25:57 -0700

    FULLY TESTED CRUD ON ALL MODELS COMPLETE

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -13,16 +13,51 @@ user_routes = Blueprint('users', __name__)
 def ebenTest():
     print("#######################################################################################################################")
 
-    # new_user = User(username='jackSparrow', email='jacksparrow@gmail.com', hashed_password='password')
+
+    ################################ TO CREATE A NEW USER
+    # new_user = User(username='lindsay', email='lindsay@gmail.com', password='password', group_id=1)
     # db.session.add(new_user)
     # db.session.commit()
 
-    # new_group = Group(name='DevOps')
-    # db.session.add(new_group)
+    ################################ TO UPDATE A USER
+    # queried_user = User.query.get(7)
+    # queried_user.group_id = None
     # db.session.commit()
 
-    # new_bug = Bug(date_created='1986-02-01', title='bug1', content='i have a problem')
+    ################################ TO DELETE A USER
+    # queried_user = User.query.get(9)
+    # db.session.delete(queried_user)
+    # db.session.commit()
+
+
+
+    ###############################################################################
+    ###############################################################################
+    ###############################################################################
+
+    ################################ TO CREATE A NEW BUGG
+    # new_bug = Bug(user_id=5, group_id=1, date_created='2000-08-12', title='Computer Broken', content='They say to turn it off and turn it on again', assignee='David', date_assigned='2000-08-13', date_resolved='2000-08-17')
     # db.session.add(new_bug)
+    # db.session.commit()
+
+    ################################ TO UPDATE A BUGG
+    # queried_bug = Bug.query.get(2)
+    # queried_bug.content = 'I hate saying that.'
+    # db.session.commit()
+
+    ################################ TO DELETE A BUGG
+    # queried_bug = Bug.query.get(2)
+    # db.session.delete(queried_bug)
+    # db.session.commit()
+
+    ###############################################################################
+    ###############################################################################
+    ###############################################################################
+
+    ################################ TO CREATE A GROUP
+
+    # new_group = Group(name='DevOps')
+    # db.session.add(new_group)
     # db.session.commit()
 
     return {"eben": "miranda"}
