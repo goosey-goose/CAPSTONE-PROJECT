7623d105c8160ca70236cf22d66a10c7fc1d9ba7
Gustavo <g.ebenezer.miranda@gmail.com>
2021-08-05 11:04:13 -0700

    FURTHER STYLE NEW BUGS LIST

--- react-app/src/components/DisplayBugInfo/DisplayBugInfo.js
+++ react-app/src/components/DisplayBugInfo/DisplayBugInfo.js
@@ -8,7 +8,7 @@ const DisplayBugInfo = () => {
   let allBugs = useSelector(state => state.allBugs.bugs);
   const dispatch = useDispatch();
 
-  console.log(allBugs[0]);
+  // console.log(allBugs[0]);
 
   useEffect(() => {
     if (!allBugs) {
@@ -23,26 +23,45 @@ const DisplayBugInfo = () => {
 
 
         <div className="main_divs" id="dbi_welcome_label">
-        Welcome, {user.username}
+          <div className="main_divs_titles">
+            Welcome, {user.username}
+          </div>
         </div>
 
         <div className="main_divs" id="dbi_new_bugs">
-        New Bugs<br></br><br></br>
+        <div className="main_divs_titles">New Bugs</div>
+          <br></br>
 
           {allBugs && <div id="dbi_new_bugs_list">
             {allBugs.map((bug, index) => (
-              <div className="dbi_single_bug" key={index}>{bug.content}</div>
+
+              <div className="dbi_single_bug" key={index}>
+                {bug.title + ":"}
+                <br></br>
+                <div className="dbi_single_bug_content">{bug.content}</div>
+                <div className="dbi_status_indicator_parent">
+                  <div className="dbi_status_indicator">
+
+                  </div>
+                </div>
+              </div>
+
+
             ))}
           </div>}
 
         </div>
 
         <div className="main_divs" id="dbi_in_progress">
-        In Progress
+          <div className="main_divs_titles">
+            In Progress
+          </div>
         </div>
 
         <div className="main_divs" id="dbi_completed_bugs">
-        Completed
+          <div className="main_divs_titles">
+            Completed
+          </div>
         </div>
 
 
