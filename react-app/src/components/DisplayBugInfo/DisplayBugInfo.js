9a5f273b6818a08d0584b9dbb8386cc1b38456aa
Gustavo <g.ebenezer.miranda@gmail.com>
2021-08-05 10:02:58 -0700

    START ADDING BUGS TO NEW BUGS LIST

--- react-app/src/components/DisplayBugInfo/DisplayBugInfo.js
+++ react-app/src/components/DisplayBugInfo/DisplayBugInfo.js
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { retrieveAllBugs } from '../../store/allBugs'
 import './DisplayBugInfo.css'
 
 const DisplayBugInfo = () => {
+  const user = useSelector(state => state.session.user);
+  let allBugs = useSelector(state => state.allBugs.bugs);
+  const dispatch = useDispatch();
+
+  console.log(allBugs[0]);
+
+  useEffect(() => {
+    if (!allBugs) {
+      dispatch(retrieveAllBugs());
+    }
+  }, [dispatch])
+
   return (
     <div id="dbi_top_div">
       <div id="dbi_grid_container">
 
 
 
-        <div className="dinosaur">
-        test
+        <div className="main_divs" id="dbi_welcome_label">
+        Welcome, {user.username}
         </div>
-        <div className="dinosaur">
-        test
+
+        <div className="main_divs" id="dbi_new_bugs">
+        New Bugs<br></br><br></br>
+
+          {allBugs && <div id="dbi_new_bugs_list">
+            {allBugs.map((bug, index) => (
+              <div className="dbi_single_bug" key={index}>{bug.content}</div>
+            ))}
+          </div>}
+
         </div>
-        <div className="dinosaur">
-        test
+
+        <div className="main_divs" id="dbi_in_progress">
+        In Progress
         </div>
-        <div className="dinosaur">
-        test
+
+        <div className="main_divs" id="dbi_completed_bugs">
+        Completed
         </div>
-        <div className="dinosaur">
-        test
-        </div>
-        <div className="dinosaur">
-        test
-        </div>
+
+
 
       </div>
     </div>
